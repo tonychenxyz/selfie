23457eb3f2bb8ed4b8cc57c1e46826fb23b7bd28
Christine Arnoldt <christine.arnoldt@ibm.com>
2024-11-28 11:09:32 +0100

    rm prints from testing

--- selfie/interpret.py
+++ selfie/interpret.py
@@ -21,7 +21,6 @@ class InterpretationPrompt:
                 # `self.tokenizer.encode()` converts  current interpretation_promp into token IDs
                 # `len(...)` gives the number of tokens so far.
                 insert_start = len(self.tokenizer.encode(self.interpretation_prompt))
-                print(self.tokenizer.encode(self.interpretation_prompt), insert_start)
                 # Modify interpretation_prompt by appending "_ " (placeholder).
                 # changes the content of the prompt and will likely change the tokenization results.
                 self.interpretation_prompt += "_ "
@@ -53,7 +52,6 @@ def interpret(original_prompt = None,
     device = torch.device("cpu")
     model.to(device)
     #
-    print(f"Interpreting '{original_prompt}' with '{interpretation_prompt.interpretation_prompt}'")
     # tokenized prompt with placeholders "_ "
     interpretation_prompt_model_inputs = interpretation_prompt.interpretation_prompt_model_inputs
     # list of positions with placeholders
@@ -101,8 +99,6 @@ def interpret(original_prompt = None,
                     insert_locations = interpretation_prompt.insert_locations
                     insert_info[layer_idx] = (insert_locations, outputs['hidden_states'][retrieve_layer][0][retrieve_token].repeat(1,len(insert_locations), 1))
             all_insert_infos.append(insert_info)
-    
-    print("all_insert_infos", all_insert_infos)
     
     # ADDED:
     torch.mps.empty_cache()
